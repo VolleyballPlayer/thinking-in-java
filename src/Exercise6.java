92cc05a48af3c8453ef474114151c4710aa9dbea
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-09 11:47:33 +0200

    Add Exercise6

---
+++ src/Exercise6.java
@@ -0,0 +1,18 @@
+public class Exercise6 {
+
+    public static void main (String[] args){
+        class StringStorage{
+            int storage(String s) {
+                return s.length() * 2;
+            }
+        }
+
+        StringStorage stringStorage = new StringStorage();
+        int result = stringStorage.storage("A");
+        System.out.println(result);
+
+        int result2 = stringStorage.storage("ABC");
+        System.out.println(result2);
+
+    }
+}
