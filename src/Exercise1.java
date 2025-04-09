31233d2486c39ef6f32ff8de1fc128bac53c6e92
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-09 09:55:32 +0200

    Add Exercise2

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,6 +1,7 @@
-public class Main {
+public class Exercise1 {
     static int i;
     static char c;
+
     public static void main(String[] args) {
         System.out.println("int = " + i);
         System.out.println("char = " + c);
