b4c2018eaf3f1dbde786d19c3961801046661053
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-09 14:18:24 +0200

    Add Exercise7

---
+++ src/Exercise7.java
@@ -0,0 +1,12 @@
+public class Exercise7 {
+    public static void main (String[] args){
+        class Incrementable {
+            static int i;
+            static void increment() {
+                i++;
+                System.out.println(i);
+            }
+        }
+        Incrementable.increment();
+    }
+}
