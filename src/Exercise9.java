8fdf257652fe659a2950a3535c1ef409dfbf9ff1
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-09 17:30:47 +0200

    Add Exercise9

---
+++ src/Exercise9.java
@@ -0,0 +1,39 @@
+public class Exercise9 {
+    public static void main (String[] args){
+        boolean b = true;
+        Boolean b_wrapper = b;
+        System.out.println(b);
+        System.out.println(b_wrapper);
+
+        byte c = 8;
+        Byte c_wrapper = c;
+        System.out.println(c);
+        System.out.println(c_wrapper);
+
+        short d = 16;
+        Short d_wrapper = d;
+        System.out.println(d);
+        System.out.println(d_wrapper);
+
+        int e = 32;
+        Integer e_wrapper = e;
+        System.out.println(e);
+        System.out.println(e_wrapper);
+
+        long f = 64;
+        Long f_wrapper = f;
+        System.out.println(f);
+        System.out.println(f_wrapper);
+
+        float g = 0.1f;
+        Float g_wrapper = g;
+        System.out.println(g);
+        System.out.println(g_wrapper);
+
+        double h = 0.11;
+        Double h_wrapper = h;
+        System.out.println(h);
+        System.out.println(h_wrapper);
+    }
+
+}
