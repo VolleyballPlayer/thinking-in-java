2e3527097f3b5011af9f9d4cf7b5429c73915fab
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-09 11:23:51 +0200

    Add Exercise5

---
+++ src/Exercise5.java
@@ -0,0 +1,17 @@
+class DataOnly {
+    int i;
+    double d;
+    boolean b;
+}
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        DataOnly data = new DataOnly();
+        data.i = 3;
+        data.d = 2.71828;
+        data.b = false;
+        System.out.println(data.i);
+        System.out.println(data.d);
+        System.out.println(data.b);
+    }
+}
