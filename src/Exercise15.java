e692311d2e937c2b57d327a4bf808c43f617f7c9
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-10 08:26:59 +0200

    Add Exercise15

---
+++ src/Exercise15.java
@@ -0,0 +1,10 @@
+/**
+ * Public class contained in file of the same name that includes main()
+ */
+public class Exercise15 {
+    /** main method executed by java
+     */
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
