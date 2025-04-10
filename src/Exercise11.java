91be261814769595bfa3b1880fcd4c93f573b18f
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-10 07:11:51 +0200

    Add Exercise11

---
+++ src/Exercise11.java
@@ -0,0 +1,23 @@
+enum Color {
+    RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET
+}
+
+class AllTheColorsOfTheRainbow {
+    int anIntegerRepresentingColors;
+    void changeTheHueOfTheColor(int newHue) {
+        Color[] colors = Color.values();
+        System.out.println("Changing color to: " + colors[newHue]);
+    }
+}
+
+public class Exercise11 {
+    public static void main (String[] args) {
+        Color[] colors = Color.values();
+        AllTheColorsOfTheRainbow obj = new AllTheColorsOfTheRainbow();
+        for (Color color : colors) {
+            obj.changeTheHueOfTheColor(color.ordinal());
+        }
+    }
+}
+
+//The hue might be the shade number of certain color, in which case this solution would look different
