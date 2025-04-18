eb59763d175538ed482c8a0b518fb167eccfc29a
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-18 20:56:20 +0200

    Add Exercise5 to operators package

---
+++ src/operators/Exercise5.java
@@ -0,0 +1,33 @@
+package operators;
+
+class Dog{
+    String name;
+    String says;
+    void setName(String n){
+        name=n;
+    }
+    void setSays(String s){
+        says=s;
+    }
+    void showDogName(){
+        System.out.println(name);
+    }
+    void speak(){
+        System.out.println(says);
+    }
+}
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        Dog spot = new Dog();
+        spot.setName("Spot");
+        spot.setSays("Ruff!");
+        Dog scruffy = new Dog();
+        scruffy.setName("Scruffy");
+        scruffy.setSays("Wurf!");
+        spot.showDogName();
+        spot.speak();
+        scruffy.showDogName();
+        scruffy.speak();
+    }
+}
