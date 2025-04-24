6bbc34ff13ffa50e1f0ce15ceeeb0f42e629331c
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-04-24 16:11:02 +0200

    Add Exercise1 to controlling_execution package

---
+++ src/controlling_execution/Exercise1.java
@@ -0,0 +1,9 @@
+package controlling_execution;
+
+public class Exercise1 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
