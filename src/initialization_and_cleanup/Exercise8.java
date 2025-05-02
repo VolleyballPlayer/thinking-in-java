7917bfb16d22eb40f2b433aab57cd3d532a123dc
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-02 09:51:08 +0200

    Add Exercise8 to initialization_and_cleanup package

---
+++ src/initialization_and_cleanup/Exercise8.java
@@ -0,0 +1,19 @@
+package initialization_and_cleanup;
+
+class Test{
+    void printWithoutThisReference(){
+        System.out.println("This is print without this reference");
+        printWithThisReference();
+        this.printWithThisReference();
+    }
+    void printWithThisReference(){
+        System.out.println("This is print with this reference");
+    }
+}
+
+public class Exercise8 {
+    public static void main(String[] args){
+        Test test = new Test();
+        test.printWithoutThisReference();
+    }
+}
