e072e1a3556bd2a091be30caf6dba2da9efa9e09
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-02 07:50:02 +0200

    Add Exercise5 to initialization_and_cleanup package

---
+++ src/initialization_and_cleanup/Exercise5.java
@@ -0,0 +1,18 @@
+package initialization_and_cleanup;
+
+class Dog {
+    void bark(int a){
+        System.out.println("Barking. Primitive type int: " + a);
+    }
+    void bark(String a){
+        System.out.println("Howling. Primitive type string: " + a);
+    }
+}
+
+public class Exercise5 {
+    public static void main(String[] args){
+        Dog sparky = new Dog();
+        sparky.bark(1);
+        sparky.bark("some_string");
+    }
+}
