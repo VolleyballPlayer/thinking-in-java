a9a45d7d4f567b57be887063e749b7f3939596b5
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-02 08:31:18 +0200

    Add Exercise7 to initialization_and_cleanup package

---
+++ src/initialization_and_cleanup/Exercise7.java
@@ -0,0 +1,13 @@
+package initialization_and_cleanup;
+
+class NoDefaultConstructor{
+    void print(){
+        System.out.println("No default constructor.");
+    }
+}
+public class Exercise7 {
+    public static void main(String[] args) {
+        NoDefaultConstructor obj = new NoDefaultConstructor();
+        obj.print();
+    }
+}
