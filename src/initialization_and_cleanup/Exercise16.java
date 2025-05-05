08da8aa65495408cdb0bb526cd6f87878e7369f2
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-05 16:53:04 +0200

    Add Exercise16 to initialization_and_cleanup package

---
+++ src/initialization_and_cleanup/Exercise16.java
@@ -0,0 +1,10 @@
+package initialization_and_cleanup;
+
+public class Exercise16 {
+    static String[] a = {"a", "b", "c"};
+    public static void main(String[] args){
+        for (String s : a) {
+            System.out.println(s);
+        }
+    }
+}
