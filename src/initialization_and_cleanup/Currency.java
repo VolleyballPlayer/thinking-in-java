f89c0a95ada5c1b56c7cc014fc8df7bcc0d9bd29
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-09 08:17:10 +0200

    Separate Currency class to initialization_and_cleanup package

---
+++ src/initialization_and_cleanup/Currency.java
@@ -0,0 +1,5 @@
+package initialization_and_cleanup;
+
+public enum Currency{
+    EUR, RSD, USD, GBP, PLN, CZK
+}
--- src/initialization_and_cleanup/Exercise21.java
+++ src/initialization_and_cleanup/Exercise21.java
@@ -1,9 +1,5 @@
 package initialization_and_cleanup;
 
-enum Currency{
-    EUR, RSD, USD, GBP, PLN, CZK
-}
-
 public class Exercise21 {
     public static void main(String[] args){
         for (Currency c: Currency.values()){
