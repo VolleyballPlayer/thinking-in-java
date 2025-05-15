869b030f5ed0bcf229324b6f6516def94878ca5d
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-15 15:08:08 +0200

    Add Exercise7 to access.control package

---
+++ src/access/control/Exercise7.java
@@ -0,0 +1,11 @@
+package access.control;
+
+import access.control.access.Widget;
+
+public class Exercise7 {
+
+    public static void main(String[] args) {
+        Widget obj = new Widget();
+    }
+
+}
