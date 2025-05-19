a00865852d311686d5c0b4dd5299f341b58b79ea
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-19 15:05:46 +0200

    Add Exercise9 to reusing.classes package

---
+++ src/reusing/classes/Exercise9.java
@@ -0,0 +1,43 @@
+package reusing.classes;
+
+class Component1{
+    Component1(){
+        System.out.println("I am Component1.");
+    }
+}
+
+class Component2{
+    Component2(){
+        System.out.println("I am Component2.");
+    }
+}
+
+class Component3{
+    Component3(){
+        System.out.println("I am Component3.");
+    }
+}
+
+class Root{
+    Root(){
+        System.out.println("I am Root.");
+        Component1 component1Root;
+        Component2 component2Root;
+        Component3 component3Root;
+    }
+}
+
+class Stem extends Root{
+    Stem(){
+        System.out.println("I am Stem.");
+        Component1 component1Stem;
+        Component2 component2Stem;
+        Component3 component3Stem;
+    }
+}
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        Stem obj = new Stem();
+    }
+}
