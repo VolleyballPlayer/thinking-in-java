1e8ae77eef8e8cbb60e4893b7ddd6d2bd6cc3df4
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-26 14:31:36 +0200

    Add Exercise16 to reusing.classes package

---
+++ src/reusing/classes/Frog.java
@@ -0,0 +1,26 @@
+package reusing.classes;
+
+class Amphibian{
+    void jump(){
+        System.out.println("Amphibian jumps");
+    }
+
+    void sleep(){
+        System.out.println("Amphibian sleeps");
+    }
+    static void eat(Amphibian a){
+        System.out.println("Amphibian eats");
+        a.sleep();
+    }
+}
+
+//Exercise16
+public class Frog extends Amphibian{
+    String type = "Frog type";
+    Frog() {this.type = type;}
+    public static void main(String[] args) {
+        Frog f = new Frog();
+        f.jump();
+        Amphibian.eat(f);
+    }
+}
