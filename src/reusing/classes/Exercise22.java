df6920662e438cc9b7544e38d679ca642db206fa
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-05-28 15:02:16 +0200

    Add Exercise22 to reusing.classes package

---
+++ src/reusing/classes/Exercise22.java
@@ -0,0 +1,14 @@
+package reusing.classes;
+
+final class FinalClass{
+    void someMethod(){
+        System.out.println("This is a method of a final class");
+    }
+}
+
+//class ToInheritFromFinal extends FinalClass{
+//}
+//It is not possible to inherit from final class
+
+public class Exercise22 {
+}
