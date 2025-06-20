555bed32de3d117a7f8d7100e1674b54bde47bc2
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-06-20 14:25:15 +0200

    Add Exercise2 to polymorphism package

---
+++ src/polymorphism/shape/Circle.java
@@ -0,0 +1,10 @@
+package polymorphism.shape;
+
+public class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Circle.draw()"); }
+    @Override
+    public void erase() {
+        System.out.println("Circle.erase()"); }
+}
