3a0340774c72bd2d087c2455a19ae4134c08c8af
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-06-20 15:55:44 +0200

    Add Exercise5 to polymorphism package

--- src/polymorphism/Exercise1.java
+++ src/polymorphism/Exercise1.java
@@ -1,26 +1,46 @@
 package polymorphism;
 
 class Cycle{
+    int numberOfWheels = 0;
     public void ride(Cycle c){
     System.out.println("Riding " + c);
+    }
+    int wheels(){
+        return numberOfWheels;
     }
 }
 
 class Unicycle extends Cycle{
+    int numberOfWheels = 1;
     public String toString() {
-        return "Unicycle";
+        return "Unicycle" ;
     }
+    @Override
+    int wheels(){
+        return numberOfWheels;
+    }
+
 }
 
 class Bicycle extends Cycle{
+    int numberOfWheels = 2;
     public String toString() {
         return "Bicycle";
+    }
+    @Override
+    int wheels(){
+        return numberOfWheels;
     }
 }
 
 class Tricycle extends Cycle{
+    private int numberOfWheels = 3;
     public String toString() {
         return "Tricycle";
+    }
+    @Override
+    int wheels(){
+        return numberOfWheels;
     }
 }
 
@@ -28,6 +48,8 @@ public class Exercise1 {
 
     static void ride(Cycle c) {
         c.ride(c);
+        int w = c.wheels();
+        System.out.println("Number of wheels is " + w);
     }
     public static void main(String[] args) {
     Unicycle unicycle = new Unicycle();
