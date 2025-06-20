bf7dbbfcc871247123eb329a969dfad6b452c694
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-06-20 15:16:49 +0200

    Add Exercise4 to polymorphism package

--- src/polymorphism/Exercise2.java
+++ src/polymorphism/Exercise2.java
@@ -5,7 +5,7 @@ public class Exercise2 {
     private static RandomShapeGenerator gen =
             new RandomShapeGenerator();
     public static void main(String[] args) {
-        Shape[] s = new Shape[9];
+        Shape[] s = new Shape[20];
 // Fill up the array with shapes:
         for(int i = 0; i < s.length; i++)
             s[i] = gen.next();
---
+++ src/polymorphism/shape/Rectangle.java
@@ -0,0 +1,10 @@
+package polymorphism.shape;
+
+public class Rectangle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Rectangle.draw()"); }
+    @Override
+    public void erase() {
+        System.out.println("Rectangle.erase()"); }
+}
