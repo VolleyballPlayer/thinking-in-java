2d2016baac199893a3b839d587f33adcf178600c
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-07-03 15:10:24 +0200

    Add Exercise2 to interfaces package

---
+++ src/interfaces/Exercise2.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+abstract class AbstractClass{}
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        //AbstractClass obj = new AbstractClass();
+    }
+
+}
