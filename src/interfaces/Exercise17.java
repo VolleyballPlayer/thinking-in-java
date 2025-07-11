ae0688e78ac285c3f9b4412f64727b3cf30dcca2
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-07-11 08:32:03 +0200

    Add Exercise17 to interfaces package

---
+++ src/interfaces/Exercise17.java
@@ -0,0 +1,23 @@
+package interfaces;
+
+interface Season {
+    String SUMMER="summer";
+    String WINTER="winter";
+    String AUTUMN="autumn";
+    String SPRING="spring";
+}
+
+class SeasonGetter implements Season{
+    String summer = Season.SUMMER;
+    String winter = Season.WINTER;
+    String autumn = Season.AUTUMN;
+    String spring = Season.SPRING;
+}
+public class Exercise17{
+    public static void main(String[] args) {
+        SeasonGetter s = new SeasonGetter();
+        System.out.println(s.summer);
+        //System.out.println(s.SUMMER);
+        //s.SUMMER = "winter";
+    }
+}
