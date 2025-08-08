601095e487cd292c9896489edcc0085834987612
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-08-08 08:09:35 +0200

    Add Exercise19 to hold objects package

---
+++ src/holdobjects/Exercise19.java
@@ -0,0 +1,26 @@
+package holdobjects;
+
+import java.util.*;
+
+public class Exercise19 {
+    public static void main(String[] args) {
+        Map<String, Integer> myMap = new HashMap<String, Integer>();
+        myMap.put("First", 1);
+        myMap.put("Second", 2);
+        myMap.put("Third", 3);
+        myMap.put("Fourth", 4);
+        System.out.println(myMap);
+
+        //Set<String> sortedKeys = new HashSet<String>(myMap.keySet());
+        Set<String> sortedKeys = new LinkedHashSet<String>(myMap.keySet());
+        Map<String, Integer> myLinkedMap = new LinkedHashMap<String, Integer>();
+
+        Iterator it = sortedKeys.iterator();
+        while (it.hasNext()){
+            String s = it.next().toString();
+            Integer i = myMap.get(s);
+            myLinkedMap.put(s, i);
+        }
+        System.out.println(myLinkedMap);
+    }
+}
