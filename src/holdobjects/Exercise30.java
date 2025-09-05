73af0dac6d29c8c5b679cf43efe2baba3cc6e0b3
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-09-05 16:22:43 +0200

    Add Exercise30 to hold objects package

---
+++ src/holdobjects/Exercise30.java
@@ -0,0 +1,123 @@
+package holdobjects;
+
+import java.util.*;
+
+class Pet{
+    int id(){
+        return 1;
+    }
+}
+
+class InterfaceVsIterator {
+    public static void display(Iterator<Pet> it) {
+        while(it.hasNext()) {
+            Pet p = it.next();
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+    }
+    public static void display(Collection<Pet> pets) {
+        for(Pet p : pets)
+            System.out.print(p.id() + ":" + p + " ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        List<Pet> petList = new ArrayList<>(8);
+        Set<Pet> petSet = new HashSet<Pet>(petList);
+        Map<String,Pet> petMap =
+                new LinkedHashMap<String,Pet>();
+        String[] names = ("Ralph, Eric, Robin, Lacey, " +
+                "Britney, Sam, Spot, Fluffy").split(", ");
+        for(int i = 0; i < names.length; i++)
+            petMap.put(names[i], petList.get(i));
+        display(petList);
+        display(petSet);
+        display(petList.iterator());
+        display(petSet.iterator());
+        System.out.println(petMap);
+        System.out.println(petMap.keySet());
+        display(petMap.values());
+        display(petMap.values().iterator());
+    }
+}
+
+public class Exercise30 implements Collection<Pet>{
+    private Pet[] pets = {new Pet(), new Pet()};
+
+    @Override
+    public int size() { return pets.length; }
+
+    @Override
+    public Iterator<Pet> iterator() {
+        return new Iterator<Pet>() {
+            private int index = 0;
+            public boolean hasNext() {
+                return index < pets.length;
+            }
+            public Pet next() { return pets[index++]; }
+            public void remove() { // not implemented
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Pet pet) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+    public static void main(String[] args) {
+        Exercise30 c = new Exercise30();
+        InterfaceVsIterator.display(c);
+        InterfaceVsIterator.display(c.iterator());
+    }
+}
