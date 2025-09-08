a811788a6ffe49d685161ffd28b8ce0fa99e2ff0
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-09-08 09:15:21 +0200

    Add Exercise32 to hold objects package

---
+++ src/holdobjects/Exercise32.java
@@ -0,0 +1,64 @@
+package holdobjects;
+
+import java.util.*;
+
+class PetSequence {
+    protected Pet[] pets = {new Pet(), new Pet()};
+}
+public class Exercise32 extends PetSequence implements Iterable<Pet> {
+    public Iterator<Pet> iterator() {
+        return new Iterator<Pet>() {
+            private int index = 0;
+            public boolean hasNext() {
+                return index < pets.length;
+            }
+            public Pet next() { return pets[index++]; }
+            public void remove() { // Not implemented
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    public Iterable<Pet> reversed() {
+        return new Iterable<Pet>() {
+            public Iterator<Pet> iterator() {
+                return new Iterator<Pet>() {
+                    int current = pets.length - 1;
+
+                    public boolean hasNext() {
+                        return current > -1;
+                    }
+
+                    public Pet next() {
+                        return pets[current--];
+                    }
+
+                    public void remove() { // Not implemented
+                        throw new UnsupportedOperationException();
+                    }
+                };
+            }
+        };
+    }
+
+    public Iterable<Pet> randomized() {
+        return new Iterable<Pet>() {
+            public Iterator<Pet> iterator() {
+                List<Pet> shuffled =
+                        new ArrayList<Pet>(Arrays.asList(pets));
+                Collections.shuffle(shuffled, new Random(47));
+                return shuffled.iterator();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        Exercise32 nc = new Exercise32();
+        for(Pet p : nc.reversed())
+            System.out.print(p + " ");
+        System.out.println();
+        for(Pet p : nc.randomized())
+            System.out.print(p + " ");
+        System.out.println();
+    }
+}
