3b6ee86d774902c76261d421ec75f3a7c315003a
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-09-23 16:08:15 +0200

    Add Exercise14 to error handling package

---
+++ src/errorhandling/Exercise14.java
@@ -0,0 +1,35 @@
+package errorhandling;
+
+class OnOffException1 extends Exception {}
+class OnOffException2 extends Exception {}
+
+class Switch {
+    private boolean state = false;
+    public boolean read() { return state; }
+    public void on() { state = true;
+        System.out.println(this); }
+    public void off() { state = false;
+        System.out.println(this); }
+    public String toString() { return state ? "on" : "off"; }
+}
+
+public class Exercise14 {
+    private static Switch sw = new Switch();
+    public static void f()
+            throws OnOffException1,OnOffException2 {}
+    public static void main(String[] args) {
+        try {
+            sw.on();
+// Code that can throw exceptions...
+            f();
+            sw.off();
+            throw new RuntimeException();
+        } catch(OnOffException1 e) {
+            System.out.println("OnOffException1");
+            sw.off();
+        } catch(OnOffException2 e) {
+            System.out.println("OnOffException2");
+            sw.off();
+        }
+    }
+}
