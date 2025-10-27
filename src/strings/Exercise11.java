a09bd268e78f8352d93d716ae6c2aeed01a999ac
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2025-10-27 15:14:51 +0100

    Add Exercise11 to strings package

---
+++ src/strings/Exercise11.java
@@ -0,0 +1,31 @@
+package strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+Args used: "Arline ate eight apples and one orange while Anita hadn’t any" "(?i)((^[aeiou])|(\s+[aeiou]))\w+?[aeiou]\b"
+*/
+
+public class Exercise11 {
+    public static void main(String[] args) {
+        if(args.length < 2) {
+            System.out.println("Usage:\njava Exercise11 " +
+                    "characterSequence regularExpression+");
+            System.exit(0);
+        }
+        System.out.println("Input: \"" + args[0] + "\"");
+        for(String arg : args) {
+            System.out.println("Regular expression: \"" + arg + "\"");
+            Pattern p = Pattern.compile(arg);
+            Matcher m = p.matcher(args[0]);
+            if(!m.find())
+                System.out.println("No match found for " + "\"" + arg + "\"");
+            m.reset();
+            while(m.find()) {
+                System.out.println("Match \"" + m.group() + "\" at position " +
+                        m.start() + ((m.end() - m.start() < 2) ? "" : ("-" + (m.end() - 1))));
+            }
+        }
+    }
+}
